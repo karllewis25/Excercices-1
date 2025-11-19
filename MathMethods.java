4865386a4976b6e6bec55cc8c4feb420501a2498
karllewis25 <karlwarrys25@gmail.com>
2025-11-19 18:53:59 +0200

    Add code for Question 8

---
+++ MathMethods.java
@@ -0,0 +1,15 @@
+public class MathMethods {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int product(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum: " + sum(7, 5));
+        System.out.println("Product: " + product(7, 5));
+    }
+}
