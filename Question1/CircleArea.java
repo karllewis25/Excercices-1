a31b8774d5c890a576677ae3b9ea1349926c5878
karllewis25 <karlwarrys25@gmail.com>
2025-11-19 18:48:49 +0200

    Add code for Question 3

---
+++ Question1/CircleArea.java
@@ -0,0 +1,10 @@
+public class CircleArea {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        double radius = 6;
+
+        double area = PI * radius * radius;
+
+        System.out.println("Area of the circle: " + area);
+    }
+}
