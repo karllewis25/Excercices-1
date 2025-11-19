cd346a655cbcffa24ea04c5827796163807eca51
karllewis25 <karlwarrys25@gmail.com>
2025-11-19 18:57:39 +0200

    Add code for Question 2

---
+++ Profile.java
@@ -0,0 +1,11 @@
+public class Profile {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 2.75;
+        boolean isPlayer = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is a Player: " + isPlayer);
+    }
+}
