d6649641f34bceb7c8635b49ef8afc654ec28fff
karllewis25 <karlwarrys25@gmail.com>
2025-11-19 18:58:07 +0200

    Add code for Question 1

---
+++ Intro.java
@@ -0,0 +1,8 @@
+
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("Name: Ossoria karl warys");
+        System.out.println("Student ID: 23146/2023");
+        System.out.println("Welcome to my world!");
+    }
+}
